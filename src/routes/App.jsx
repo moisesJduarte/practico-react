16f64656bfc9041dbece10b777ec266aadcc1b0d
moisesJduarte <gladmd19@gmail.com>
2022-09-13 21:12:28 -0400

    adding changing

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
 import NewPassword from '../containers/NewPassword';
+import Confirm from '../containers/Confirm';
 import RecoveryPassword from '../containers/RecoveryPassword';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
@@ -13,6 +14,8 @@ const App = () => {
 			<Layout>
 				<Switch>
 					<Route exact path="/" component={Home} />
+					<Route exact path="/Confirm" component={Confirm} />
+
 					<Route exact path="/NewPassword" component={NewPassword} />
 					<Route exact path="/recovery-password" component={RecoveryPassword} />
 					<Route path="*" component={NotFound} />
