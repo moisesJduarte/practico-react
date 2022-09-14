c74c9163a9d4fb319f21194aa5268020801d677d
moisesJduarte <gladmd19@gmail.com>
2022-09-13 21:32:32 -0400

    adding the other tamplate

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
-import NewPassword from '../containers/NewPassword';
-import Confirm from '../containers/Confirm';
-import RecoveryPassword from '../containers/RecoveryPassword';
+import NewPassword from '../pages/NewPassword';
+import Confirm from '../pages/Confirm';
+import RecoveryPassword from '../pages/RecoveryPassword';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 import '../styles/global.css';
