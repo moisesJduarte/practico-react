e470c4072f2f99b064a162638b4fc05905ddcdc4
moisesJduarte <gladmd19@gmail.com>
2022-09-18 17:37:39 -0400

    adding alias

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,46 +1,48 @@
-import React from "react";
+import React from 'react';
 import '@styles/Header.scss';
 
-const logo = '@icons/icon_menu.svg';
+import menu from '@icons/icon_menu.svg';
+import logo from '@logos/logo_yard_sale.svg';
+import shoppingCart from '@icons/icon_shopping_cart.svg';
 
-const Headre = () => {
-  return (
-    <nav>
-      <img src={logo} alt="menu" className="menu" />
-      <div className="navbar-left">
-        <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
-        <ul>
-          <li>
-            <a href="/">All</a>
-          </li>
-          <li>
-            <a href="/">Clothes</a>
-          </li>
-          <li>
-            <a href="/">Electronics</a>
-          </li>
-          <li>
-            <a href="/">Furnitures</a>
-          </li>
-          <li>
-            <a href="/">Toys</a>
-          </li>
-          <li>
-            <a href="/">Others</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-right">
-        <ul>
-          <li className="navbar-email">platzi@example.com</li>
-          <li className="navbar-shopping-cart">
-            <img src="./icons/icon_shopping_cart.svg" alt="shopping cart" />
-            <div>2</div>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
-};
+const Header = () => {
+	return (
+		<nav>
+			<img src={menu} alt="menu" className="menu" />
+			<div className="navbar-left">
+				<img src={logo} alt="logo" className="nav-logo" />
+				<ul>
+					<li>
+						<a href="/">All</a>
+					</li>
+					<li>
+						<a href="/">Clothes</a>
+					</li>
+					<li>
+						<a href="/">Electronics</a>
+					</li>
+					<li>
+						<a href="/">Furnitures</a>
+					</li>
+					<li>
+						<a href="/">Toys</a>
+					</li>
+					<li>
+						<a href="/">Others</a>
+					</li>
+				</ul>
+			</div>
+			<div className="navbar-right">
+				<ul>
+					<li className="navbar-email">platzi@example.com</li>
+					<li className="navbar-shopping-cart">
+						<img src={shoppingCart} alt="shopping cart" />
+						<div>2</div>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+}
 
-export default Headre;
+export default Header;
