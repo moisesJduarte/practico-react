2c0ab4a59cb92573a2b28d65409ba471c31672e7
moisesJduarte <gladmd19@gmail.com>
2022-09-22 23:41:42 -0400

    tarbajndo en el SASS y agregando funcion al Cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
 import React, {useState, useContext } from 'react';
 import '@styles/Header.scss';
 import Menu from '@components/Menu';
+import myOder from '../containers/MyOrder';
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import AppContext from '../context/AppContext'; 
 import shoppingCart from '@icons/icon_shopping_cart.svg';
+import MyOrder from '../containers/MyOrder';
 
 const Header = () => {
 	const [toggle, setToggle] = useState(false);
+	const [toggleOders, setToggleOrders] = useState(false);
 	const { state } = useContext(AppContext);
+	
 
 	const handleToggle = () => {
 	setToggle(!toggle);
@@ -45,13 +49,17 @@ const Header = () => {
 					<li className="navbar-email" onClick={handleToggle}>
 						Gladmd19@gmail.com
 					</li>
-					<li className="navbar-shopping-cart">
+					<li 
+					className="navbar-shopping-cart" 
+					onClick={() => setToggleOrders (!toggleOders)}
+					>
 						<img src={shoppingCart} alt="shopping cart" />
 						{state.cart.length > 0 ? <div>{state.cart.length}</div> : null }
 					</li>
 				</ul>
 			</div>
 			{toggle && <Menu />}
+			{toggleOders && <MyOrder />}
 		</nav>
 	);
 }
