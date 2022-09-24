e9c287e856647942d99108b97643d08b929f3716
moisesJduarte <gladmd19@gmail.com>
2022-09-23 22:35:28 -0400

    dominio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React, {useState, useContext } from 'react';
 import '@styles/Header.scss';
 import Menu from '@components/Menu';
-import myOder from '../containers/MyOrder';
+import MyOrder from '../containers/MyOrder';
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import AppContext from '../context/AppContext'; 
 import shoppingCart from '@icons/icon_shopping_cart.svg';
-import MyOrder from '../containers/MyOrder';
+
 
 const Header = () => {
 	const [toggle, setToggle] = useState(false);
